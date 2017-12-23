c5c48aeb9069a7f1022fdee6dbcc4e387c1881ac
Sam Ageloff <sam@getambassador.com>
2017-12-23 08:30:51 -0500

    firing up parcel

---
+++ index.js
@@ -0,0 +1,9 @@
+import './styles.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => <h1>Core.ly</h1>
+
+const rootDiv = document.getElementById('root');
+
+ReactDOM.render(<App />, rootDiv);
